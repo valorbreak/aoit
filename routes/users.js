92592038011d550fabd8a5b79fc0d813dba430ff
Uri Acuesta <acuestal@hhmi.org>
2014-04-13 23:46:20 -0400

    Updates

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/what', function(req, res) {
+  res.send('respond with a what');
+});
+
 module.exports = router;
